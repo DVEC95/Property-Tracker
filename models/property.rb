22d3718a86469279556bb0c70260176b04136bae
David Eastcroft <davideastcroft95@gmail.com>
2020-03-24 18:40:21 +0000

    Adds ID and fixes save method.

--- models/property.rb
+++ models/property.rb
@@ -3,12 +3,14 @@ require ('pg')
 class Property
 
   attr_accessor :address, :value, :bedrooms, :buy_let_status
+  attr_reader :id
 
   def initialize(properties)
     @address = properties['address']
     @value = properties['value']
     @bedrooms = properties['bedrooms']
     @buy_let_status = properties['buy_let_status']
+    @id = properties['id'].to_i if properties['id']
   end
 
   def save()
@@ -19,8 +21,15 @@ class Property
         value,
         bedrooms,
         buy_let_status
-      )"
-    # db.prepare("save", sql)
+      )
+      VALUES
+      (
+        $1, $2, $3, $4
+      )
+      RETURNING *"
+    values = [@address, @value, @bedrooms, @buy_let_status]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]["id"].to_i
     db.close
   end
 
