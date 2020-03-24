6ec745b4f0797ddae6b08460eefea1b91098b04f
David Eastcroft <davideastcroft95@gmail.com>
2020-03-24 18:30:51 +0000

    Adds sample data.

---
+++ db/console.rb
@@ -0,0 +1,29 @@
+require('pry-byebug')
+require_relative('../models/property.rb')
+
+property1 = Property.new({
+    'address' => '43 Hall Drive',
+    'value' => 60_000,
+    'bedrooms' => 2,
+    'buy_let_status' => 'to let'
+})
+
+property2 = Property.new({
+    'address' => 'Castle Hill House',
+    'value' => 200_000,
+    'bedrooms' => 4,
+    'buy_let_status' => 'to buy'
+})
+
+property3 = Property.new({
+    'address' => '240 Priory Road',
+    'value' => 360_000,
+    'bedrooms' => 5,
+    'buy_let_status' => 'to buy'
+})
+
+property1.save()
+property2.save()
+
+binding.pry
+nil
